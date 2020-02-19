9bf36741ce78ca2b26d7a46fe4362e7c8e93ac97
Zac Holland <zachary@pubnub.com>
2020-02-19 09:42:40 -0800

    added config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,20 @@
-const dotenv = require('dotenv')
-dotenv.config()
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 module.exports = {
   env: {
     AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
     AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
     AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
-    AUTH0_SCOPE: 'openid profile',
+    AUTH0_SCOPE: "openid profile offline_access",
+    API_AUDIENCE: process.env.API_AUDIENCE,
+    API_BASE_URL: "http://localhost:3001",
     REDIRECT_URI:
-      process.env.REDIRECT_URI || 'http://localhost:3000/api/callback',
+      process.env.REDIRECT_URI || "http://localhost:3000/api/callback",
     POST_LOGOUT_REDIRECT_URI:
-      process.env.POST_LOGOUT_REDIRECT_URI || 'http://localhost:3000/',
+      process.env.POST_LOGOUT_REDIRECT_URI || "http://localhost:3000/",
     SESSION_COOKIE_SECRET: process.env.SESSION_COOKIE_SECRET,
-    SESSION_COOKIE_LIFETIME: 7200, // 2 hours
-  },
-}
+    SESSION_COOKIE_LIFETIME: 7200 // 2 hours
+  }
+};
